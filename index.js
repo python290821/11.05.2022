98dea234cd4806b884e8e297b2882adcde70fe00
python290821 <python290821@gmail.com>
2022-05-11 19:06:25 +0300

    upload all

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const winston = require('winston');
+//const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
+
+a =  { x : 5, y: 10, z: 12 }
+//const { x } = a; // x = a.x
+//const x = a.x
+const { x, y, z } = a;
 
 const logConfiguration =  {
+    format: combine(
+        label({label: 'right meow!'}),
+        timestamp(),
+        prettyPrint()
+    ),
     'transports': [
         // save to console
-        new winston.transports.Console()
+        new transports.Console(),
+        new transports.File({ filename: 'exmaple.log' })
     ]
 }
 
-const logger = winston.createLogger(logConfiguration);
+const logger = createLogger(logConfiguration);
 
 logger.info('Hello Winston!');
 
